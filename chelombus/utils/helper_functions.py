142763c9eb2412c283895102ed32b35d7aea6aa1
afloresep <afloresep01@gmail.com>
2024-11-21 13:11:36 +0100

    (feat) process input method for handling multiple files instead of single files

--- chelombus/utils/helper_functions.py
+++ chelombus/utils/helper_functions.py
@@ -1,14 +1,4 @@
-import logging
-from config import LOGGING_LEVEL, LOGGING_FORMAT, LOG_FILE_PATH
-
-def setup_logging():
-    logging.basicConfig(
-        level=getattr(logging, LOGGING_LEVEL),
-        format=LOGGING_FORMAT,
-        filename=LOG_FILE_PATH  # To log to a file
-    )
-
-
+import os
 
 def find_input_type(file_path):
       if file_path.endswith('csv'):
@@ -20,9 +10,16 @@ def find_input_type(file_path):
       else: 
             raise ValueError('Unsupported input file. Only .csv, .txt. and .cxsmiles files are supported')
 
-def validate_data():
-    pass
-
-def i_o_operations():
-    pass
+def process_input(input_path):
+    if os.path.isdir(input_path):
+        # Process all files in the directory
+        for file in os.listdir(input_path):
+            file_path = os.path.join(input_path, file)
+            if os.path.isfile(file_path):  # Optional: filter by file extensions
+                yield file_path
+    elif os.path.isfile(input_path):
+        # Process a single file
+        yield input_path
+    else:
+        raise ValueError(f"Invalid input path: {input_path}")
 
