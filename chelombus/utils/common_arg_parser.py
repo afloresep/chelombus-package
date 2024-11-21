a0f0361d345d54c499e0f7da67468bef818e2616
afloresep <afloresep01@gmail.com>
2024-11-21 13:11:03 +0100

    adding common arg parser

---
+++ chelombus/utils/common_arg_parser.py
@@ -0,0 +1,13 @@
+import argparse
+from chelombus.utils.config import LOGGING_LEVEL, N_JOBS
+
+def common_arg_parser(description):
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument("--config", type=str, help="Path to the user configuration file.")
+    parser.add_argument('--log-level', type=str, default=LOGGING_LEVEL,
+                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help="Logging verbosity level.")
+    parser.add_argument('--n-jobs', type=int, default=N_JOBS, help="Number of CPU cores to use.")
+    parser.add_argument('--resume', type=int, default=0, help="Resume from a specific chunk.")
+    parser.add_argument('--log', type=bool, default=False, help="Saving logs to output.log file. Default False")
+    
+    return parser
