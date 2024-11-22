8238210488358c62357ba4817c7ea4c9464820ce
afloresep <afloresep01@gmail.com>
2024-11-22 20:56:56 +0100

    fix imports

--- chelombus/__init__.py
+++ chelombus/__init__.py
@@ -1,10 +1,8 @@
 from .data_handler import DataHandler
 from .fingerprint_calculator import FingerprintCalculator
-from .dimensionality_reducer import DimensionalityReducer
 from .output_generator import OutputGenerator
 
-
-# When using <<from src import * >> only the classes listed in __all__ will be imported.
+# When using <<from chelombus import * >> only the classes listed in __all__ will be imported.
 __all__ = [
     'DataHandler',
     'FingerprintCalculator',
