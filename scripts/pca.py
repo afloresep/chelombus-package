319f95b40119425ecbbdee42e0f62d795cdc4fcf
afloresep <afloresep01@gmail.com>
2024-11-24 14:20:14 +0100

    feat loading ipca model

--- scripts/pca.py
+++ scripts/pca.py
@@ -2,6 +2,7 @@
 import sys
 import time
 import logging 
+import joblib
 from pydantic import ValidationError
 import tqdm 
 import pandas as pd 
@@ -23,9 +24,9 @@ def main() -> None:
    parser.add_argument("--chunksize", type=int, help="Override config chunksize.")
    parser.add_argument('--pca-components', type=int, help="Number of PCA components.")
    parser.add_argument('--header', type=int, help='Row number(s) containing column labels and marking the start of the data (zero-indexed). Default header is first row (header=0)')
-   parser.add_argument('--smiles_col_idx', type=int, help='Column index for the smiles values in the input data. Default is first column (0)')
-   parser.add_argument('--resume_chunk', type=int, default=0, help="Resume from a specific chunk.")
-   parser.add_argument('--model', type=str, help='Load a PCA model using joblib to avoid the fitting and directly transform the result')
+   parser.add_argument('--smiles-col-idx', type=int, help='Column index for the smiles values in the input data. Default is first column (0)')
+   parser.add_argument('--resume-chunk', type=int, default=0, help="Resume from a specific chunk.")
+   parser.add_argument('--ipca-model', default=None, type=str, help='Load a PCA model using joblib to avoid the fitting and directly transform the result')
    args = parser.parse_args() 
 
    #Set up logging
@@ -45,7 +46,6 @@ def main() -> None:
    "CHUNKSIZE":args.chunksize or config.CHUNKSIZE,
    "PCA_N_COMPONENTS": args.pca_components or config.PCA_N_COMPONENTS,
    "N_JOBS": args.n_jobs or config.N_JOBS,
-   "PCA_MODEL": args.model or config.get("MODEL"),
    }) 
 
    os.makedirs(config.OUTPUT_PATH, exist_ok=True)
@@ -58,32 +58,40 @@ def main() -> None:
 
    start = time.time()
 
-   ipca = IncrementalPCA(pca_n_components=config.PCA_N_COMPONENTS) 
-   for file_path in process_input(config.INPUT_PATH):
-      data_handler = DataHandler(file_path=file_path, chunksize=config.CHUNKSIZE, smiles_col_index=1, header=None) # TODO: Think best way to solve when user has different col_index and wants to change it. 
-      output_gen = OutputGenerator()
+   if args.ipca_model != None:
+      try:
+         ipca = joblib.load(args.ipca_model)
+      except Exception as e:
+         logging.error(f"iPCA model {args.ipca_model} could not be loaded. Error {e}") 
+         sys.exit(1)
+   else:
+      ipca = IncrementalPCA(n_components=config.PCA_N_COMPONENTS) 
+      for file_path in process_input(config.DATA_PATH):
+         # TODO: Think best way to solve when user has different col_index and wants to change it. 
+         data_handler = DataHandler(file_path=file_path, chunksize=config.CHUNKSIZE, smiles_col_index=1, header=None) 
+         output_gen = OutputGenerator()
 
-      total_chunks = data_handler.get_total_chunks()
+         total_chunks = data_handler.get_total_chunks()
 
-      start = time.time()
-      for idx in tqdm(range(args.resume_chunk,total_chunks), desc="Loading Fingerprints and iPCA partial fitting"):
-         try:
-            fp_chunk_path = os.path.join(config.OUTPUT_PATH, f'batch_parquet/fingerprints_chunk_{idx}.parquet') 
-            df_fingerprints = pd.read_parquet(fp_chunk_path, engine="pyarrow")
+         start = time.time()
+         for idx in tqdm(range(args.resume_chunk,total_chunks), desc="Loading Fingerprints and iPCA partial fitting"):
+            try:
+               fp_chunk_path = os.path.join(config.OUTPUT_PATH, f'batch_parquet/fingerprints_chunk_{idx}.parquet') 
+               df_fingerprints = pd.read_parquet(fp_chunk_path, engine="pyarrow")
 
-            fingerprints_columns = df_fingerprints.drop(columns=['smiles']).values
-            ipca.partial_fit(fingerprints_columns)
+               fingerprints_columns = df_fingerprints.drop(columns=['smiles']).values
+               ipca.partial_fit(fingerprints_columns)
 
-            del df_fingerprints, fingerprints_columns
-            gc.collect()
-         except Exception as e: 
-            logging.error(f"Error during PCA fitting for chunk {idx}: {e}", exc_info=True)
-      end = time.time()
-      logging.info(f"iPCA fitting done in {config.INPUT_PATH} took {int((end - start) // 3600)} hours, {int(((end - start) % 3600) // 60)} minutes, and {((end - start) % 60):.2f} seconds")
+               del df_fingerprints, fingerprints_columns
+               gc.collect()
+            except Exception as e: 
+               logging.error(f"Error during PCA fitting for chunk {idx}: {e}", exc_info=True)
+         end = time.time()
+         logging.info(f"iPCA fitting done in {config.DATA_PATH} took {int((end - start) // 3600)} hours, {int(((end - start) % 3600) // 60)} minutes, and {((end - start) % 60):.2f} seconds")
 
-   for file_path in process_input(config.INPUT_PATH):
+   for file_path in process_input(config.DATA_PATH):
       data_handler = DataHandler(file_path=file_path, chunksize=config.CHUNKSIZE, smiles_col_index=1, header=None) # TODO: Think best way to solve when user has different col_index and wants to change it.
-      total_chunks = data_handler.get_total_lines(9)
+      total_chunks = data_handler.get_total_lines()
       start = time.time()
 
       for idx in tqdm(range(args.resume_chunk, total_chunks), desc='iPCA transform and saving results'):
@@ -105,7 +113,7 @@ def main() -> None:
             logging.error(f"Error during data transformation for chunk {idx}: {e}", exc_info=True)
 
       end = time.time()
-      logging.info(f"iPCA fitting done in {config.INPUT_PATH} took {int((end - start) // 3600)} hours, {int(((end - start) % 3600) // 60)} minutes, and {((end - start) % 60):.2f} seconds")
+      logging.info(f"iPCA fitting done in {config.DATA_PATH} took {int((end - start) // 3600)} hours, {int(((end - start) % 3600) // 60)} minutes, and {((end - start) % 60):.2f} seconds")
 
    end = time.time()
-   logging.info(f"iPCA calculations for {config.INPUT_PATH} took {int((end - start) // 3600)} hours, {int(((end - start) % 3600) // 60)} minutes, and {((end - start) % 60):.2f} seconds")
+   logging.info(f"iPCA calculations for {config.DATA_PATH} took {int((end - start) // 3600)} hours, {int(((end - start) % 3600) // 60)} minutes, and {((end - start) % 60):.2f} seconds")
