4b5b4ad3e24f426731d5f5b99215b3eeb3ab2cb7
afloresep <afloresep01@gmail.com>
2024-11-29 12:19:53 +0100

    fix file loading

--- scripts/pca.py
+++ scripts/pca.py
@@ -10,23 +10,23 @@ import os
 from chelombus.utils.log_setup import setup_logging
 from chelombus.utils.common_arg_parser import common_arg_parser
 from chelombus.utils.config_loader import load_config
-from chelombus.utils.helper_functions import process_input, TimeTracker, RAMAndTimeTracker, FileProgressTracker
+from chelombus.utils.helper_functions import process_input, TimeTracker , RAMAndTimeTracker, FileProgressTracker
 from chelombus.data_handler import DataHandler  
 from chelombus.output_generator import OutputGenerator
 from sklearn.decomposition import IncrementalPCA
 import gc
 
-# Example usage in a CLI script
 def main() -> None:
    parser = common_arg_parser(description="Calculate iPCA from fingerprints")
-   parser.add_argument('--input-path', help="Input file or directory.")
-   parser.add_argument('--output-dir', help="Directory to save fingerprints.")
+   parser.add_argument('--data-path', help="Input data file or directory.")
+   parser.add_argument('--output-path', help="Directory to save fingerprints.")
    parser.add_argument("--chunksize", type=int, help="Override config chunksize.")
    parser.add_argument('--pca-components', type=int, help="Number of PCA components.")
    parser.add_argument('--header', type=int, help='Row number(s) containing column labels and marking the start of the data (zero-indexed). Default header is first row (header=0)')
    parser.add_argument('--smiles-col-idx', type=int, help='Column index for the smiles values in the input data. Default is first column (0)')
    parser.add_argument('--resume-chunk', type=int, default=0, help="Resume from a specific chunk.")
    parser.add_argument('--ipca-model', default=None, type=str, help='Load a PCA model using joblib to avoid the fitting and directly transform the result')
+   parser.add_argument('--remove', default=False, type=bool, help='Remove fingerprints files after PCA reduction')
    args = parser.parse_args() 
 
    start = time.time()
@@ -42,8 +42,8 @@ def main() -> None:
 
    # Override configuration with CLI args if provided
    config = config.copy(update={
-   "DATA_PATH":args.input_path or config.DATA_PATH,
-   "OUTPUT_PATH":args.output_dir or config.OUTPUT_PATH,
+   "DATA_PATH":args.data_path or config.DATA_PATH,
+   "OUTPUT_PATH":args.output_path or config.OUTPUT_PATH,
    "CHUNKSIZE":args.chunksize or config.CHUNKSIZE,
    "PCA_N_COMPONENTS": args.pca_components or config.PCA_N_COMPONENTS,
    "N_JOBS": args.n_jobs or config.N_JOBS,
@@ -59,11 +59,16 @@ def main() -> None:
    logging.info(f"Using {config.N_JOBS} CPU cores")
    logging.info(f"Number of PCA Components: {config.PCA_N_COMPONENTS}")
 
-   with FileProgressTracker(description="Fitting", total_files=35) as tracker:
+   # This is just to ensure that in case a file is provided as input instead of a folder, Progress bar also works
+   if os.path.isdir(config.DATA_PATH):
+      total_files = len(os.listdir(config.DATA_PATH))
+   else:
+      total_files = 1 
+
+   with FileProgressTracker(description="Fitting", total_files=total_files) as tracker:
       if config.IPCA_MODEL == None:
          ipca = IncrementalPCA(n_components=config.PCA_N_COMPONENTS) 
          for idx, file_path in enumerate(process_input(config.DATA_PATH)):
-            time.sleep(2)
             # TODO: Think best way to solve when user has different col_index and wants to change it. 
             try:
                df_fingerprints = pd.read_parquet(file_path, engine="pyarrow")
@@ -81,17 +86,13 @@ def main() -> None:
             ipca = joblib.load(args.ipca_model)
          except Exception as e:
             logging.error(f"iPCA model {args.ipca_model} could not be loaded. Error {e}") 
-         sys.exit(1)
+            sys.exit(1)
 
-   for file_path in process_input(config.DATA_PATH):
-      total_chunks = DataHandler.get_total_lines()
-      start = time.time()
-
-      for idx in tqdm(range(args.resume_chunk, total_chunks), desc='iPCA transform and saving results'):
+   with FileProgressTracker(description="Transforming and saving PCA results", total_files=len(os.listdir(config.DATA_PATH))) as tracker:
+      for file_path in process_input(config.DATA_PATH):
          try:
             # Load fingerprint
-            fp_chunk_path = os.path.join(config.OUTPUT_PATH, f'batch_parquet/fingerprints_chunk_{idx}.parquet')
-            df_fingerprints  =  pd.read_parquet(fp_chunk_path)
+            df_fingerprints  =  pd.read_parquet(file_path)
             features = []
             coordinates = ipca.transform(df_fingerprints.drop(columns=['smiles']).values)  # -> np.array shape (chunk_size, n_pca_comp)
 
@@ -100,13 +101,11 @@ def main() -> None:
 
             # Free memory space
             del df_fingerprints, coordinates, features 
-            # os.remove(fp_chunk_path) 
-
+            if args.remove:
+               os.remove(file_path)
+   
          except Exception as e:
-            logging.error(f"Error during data transformation for chunk {idx}: {e}", exc_info=True)
-
-      end = time.time()
-      logging.info(f"iPCA fitting done in {config.DATA_PATH} took {int((end - start) // 3600)} hours, {int(((end - start) % 3600) // 60)} minutes, and {((end - start) % 60):.2f} seconds")
+               logging.error(f"Error during data transformation for chunk {idx}: {e}", exc_info=True)
 
    end = time.time()
-   logging.info(f"iPCA calculations for {config.DATA_PATH} took {int((end - start) // 3600)} hours, {int(((end - start) % 3600) // 60)} minutes, and {((end - start) % 60):.2f} seconds")
+   logging.info(f"iPCA calculations for {config.DATA_PATH} took {TimeTracker.format_time(end - start)}")
