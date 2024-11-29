d4ef9a35669f978bacb5a1704955f29032c111ee
afloresep <afloresep01@gmail.com>
2024-11-29 12:20:07 +0100

    feat fp pipeline

--- scripts/fingerprint.py
+++ scripts/fingerprint.py
@@ -1,66 +1,67 @@
 # CLI for fingerprint calculation on billions of molecules
-
+import sys
 import time
 import logging 
+from pydantic import ValidationError
 import tqdm 
+import pandas as pd 
 import os
-from chelombus.data_handler import DataHandler
 from chelombus.utils.log_setup import setup_logging
 from chelombus.utils.common_arg_parser import common_arg_parser
 from chelombus.utils.config_loader import load_config
-from chelombus.utils.helper_functions import process_input, TimeTracker
+from chelombus.utils.helper_functions import process_input, TimeTracker , FileProgressTracker
+from chelombus.data_handler import DataHandler  
 
-# Example usage in a CLI script
 def main() -> None:
-    parser = common_arg_parser(description="Calculate fingerprints")
-    parser.add_argument('--input-path', help="Input file or directory.")
-    parser.add_argument('--output-dir', help="Directory to save fingerprints.")
-    parser.add_argument("--fingerprint-type", choices=["mqn", "morgan", "mhfp", "mapc"], default="mqn",
-                        help="Type of fingerprint to calculate.")
-    parser.add_argument("--fp-size", type=int, default=1024, help="Size of fingerprint vectors.")
-    parser.add_argument("--chunksize", type=int, help="Override config chunksize.")
-    args = parser.parse_args() 
+   parser = common_arg_parser(description="Calculate iPCA from fingerprints")
+   parser.add_argument('--data-path', help="Input data file or directory.")
+   parser.add_argument('--output-path', help="Directory to save fingerprints.")
+   parser.add_argument("--chunksize", type=int, help="Override config chunksize.")
+   parser.add_argument('--resume-chunk', type=int, default=0, help="Resume from a specific chunk.")
+   parser.add_argument('--n-jobs', type=int, help='Number of CPU cores to be used. Default uses all available CPU cores')
+   args = parser.parse_args() 
 
-    #Set up logging
-    setup_logging(args.log_level)
+   start = time.time()
+   
+   #Set up logging
+   setup_logging(args.log_level)
 
-    # Load configuration
-    config = load_config(args.config)
-    logging.info("Configuration loaded successfully")
+   # Load configuration
+   try:
+      config = load_config(args.config)
+   except ValidationError as e:
+      logging.error(f"Configuration error: {e}")
+      sys.exit(1)
 
-    # Override configuration with CLI args if provided
-    config["DATA_PATH"] = args.input_path or config.get("DATA_PATH")
-    config["OUTPUT_PATH"] = args.output_dir or config.get("OUTPUT_PATH")
-    config["CHUNKSIZE"] = args.chunksize or config.get("CHUNKSIZE")
-    config["N_JOBS"] = args.n_jobs or config.get("N_JOBS")
-    output_dir = os.path.abspath(args.output_dir)
-    os.makedirs(output_dir, exist_ok=True)
+   # Override configuration with CLI args if provided
+   config = config.copy(update={
+   "DATA_PATH":args.data_path or config.DATA_PATH,
+   "OUTPUT_PATH":args.output_path or config.OUTPUT_PATH,
+   "CHUNKSIZE":args.chunksize or config.CHUNKSIZE,
+   "N_JOBS": args.n_jobs or config.N_JOBS,
+   }) 
 
-    input_path = config["DATA_PATH"]
-    output_dir = config["OUTPUT_PATH"]
-    chunksize = config["CHUNKSIZE"]
-    n_jobs = config["N_JOBS"]
+   os.makedirs(config.OUTPUT_PATH, exist_ok=True)
 
-    logging.info(f"Input path: {input_path}")
-    logging.info(f"Output directory: {output_dir}")
-    logging.info(f"Chunk size: {chunksize}")
-    logging.info(f"Using {n_jobs} CPU cores")
+   logging.info(f"Input path: {config.DATA_PATH}")
+   logging.info(f"Output directory: {config.OUTPUT_PATH}")
+   logging.info(f"Chunksize: {config.CHUNKSIZE}")
+   logging.info(f"Using {config.N_JOBS} CPU cores")
 
-    with TimeTracker(description='Fingerprints calculations'):
-        for file_path in process_input(input_path):
-            logging.info(f"Processing file: {file_path}")
-            # Process the file (e.g., calculate fingerprints)
-            data_handler = DataHandler(file_path, chunksize=chunksize)
-            data_chunks, total_chunks = data_handler.load_data()
+   # This is just to ensure that in case a file is provided as input instead of a folder, Progress bar also works
+   if os.path.isdir(config.DATA_PATH):
+      total_files = len(os.listdir(config.DATA_PATH))
+   else:
+      total_files = 1 
 
-            start = time.time()
-            for idx, chunk in enumerate(tqdm(data_chunks, total= total_chunks, desc=f"Loading chunk and calculating its fingerprints")):
-                data_handler.process_chunk(idx, chunk, output_dir)
-                del idx, chunk
-            logging.info(f"Calculations for {file_path} took {int((end - start) // 3600)} hours, {int(((end - start) % 3600) // 60)} minutes, and {((end - start) % 60):.2f} seconds")
-  
+   with FileProgressTracker(description="Calculating Fingerprints", total_files=total_files) as tracker:
+    for file_path in process_input(config.DATA_PATH):
+        data_handler = DataHandler(file_path, chunksize=config.CHUNKSIZE)
+        data_chunks, total_chunks= data_handler.load_data()
 
-    # Process chunks with tqdm progress bar
-
-    end = time.time()    
-    logging.info(f"Fingerprints calculations for {input_path} took {int((end - start) // 3600)} hours, {int(((end - start) % 3600) // 60)} minutes, and {((end - start) % 60):.2f} seconds")
+        for idx, chunk in enumerate(tqdm(data_chunks, total= total_chunks, desc=f"Loading chunk and calculating its fingerprints")):
+            data_handler.process_chunk(idx, chunk, config.OUTPUT_PATH)
+            del idx, chunk
+        tracker.update_progress()
+   end = time.time()
+   logging.info(f"Fingerprint calculation for {config.DATA_PATH} took: {TimeTracker.format_time(end -start)}")
