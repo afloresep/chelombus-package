5ffa72164b37d7b8dd9dba76d8d8c9210c2249b6
afloresep <afloresep01@gmail.com>
2024-12-19 13:42:21 +0100

    improve comments and naming

--- cli/clustering.py
+++ cli/clustering.py
@@ -1,16 +1,19 @@
-# CLI for clustering using TDigest and Spark 
+# CLI for clustering using Dask and Spark. So far the fastest (and efficient) way of doing it. 
 # CLI for fingerprint calculation on billions of molecules
 import sys
-import pandas as pd
 import logging 
 import os
-import pickle
-from tdigest import TDigest
+import dask.dataframe as dd
 from pydantic import ValidationError
+from api.dask_clustering import Cluster
 from api.utils.log_setup import setup_logging
 from api.utils.common_arg_parser import common_arg_parser
 from api.utils.config_loader import load_config
-from api.utils.helper_functions import process_input, FileProgressTracker
+from api.utils.helper_functions import process_input, ProgressTracker
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, concat_ws, ntile, lit
+from pyspark.sql.window import Window
+
 
 def main() -> None:
     parser = common_arg_parser(description="Clustering from PCA using TDigest")
@@ -34,8 +37,7 @@ def main() -> None:
     config = config.copy(update={
     "DATA_PATH":args.data_path or config.DATA_PATH,
     "OUTPUT_PATH":args.output_path or config.OUTPUT_PATH,
-    "CHUNKSIZE":args.chunksize or config.CHUNKSIZE,
-    "N_JOBS": args.n_jobs or config.N_JOBS,
+    "STEPS_LIST":args.steps_list or config.STEPS_LIST,
     }) 
 
     os.makedirs(config.OUTPUT_PATH, exist_ok=True)
@@ -43,28 +45,88 @@ def main() -> None:
     logging.info(f"Input path: {config.DATA_PATH}")
     logging.info(f"Output directory: {config.OUTPUT_PATH}")
     logging.info(f"TDigest Model loaded: {config.TDIGEST_MODEL}")
+    logging.info(f"Number of steps: {config.STEPS_LIST}")
+    # TODO: Add Spark RAM config 
+    # TODO: Add tmp folder option, right now is on same path as OUTPUT_PATH
+ 
+    # Initialize Cluster class.
+    cluster = Cluster()
 
-    # This is just to ensure that in case a file is provided as input instead of a folder, Progress bar also works
-    if os.path.isdir(config.DATA_PATH):
-        total_files = len(os.listdir(config.DATA_PATH))
-    else:
-        total_files = 1 
+    ## Read the Dataframe containing the PCA coordinates
+    pca_dataframe = dd.read_parquet(config.DATA_PATH, engine="pyarrow")
 
-    if args.model == None: 
-        digest_model = TDigest()
-            
-        with FileProgressTracker(description="Calculating Fingerprints", total_files=total_files) as tracker:
-            for file_path in process_input(config.DATA_PATH):
-                parquet_dataframe = pd.read_parquet(file_path)
-                digest_model.batch_update(parquet_dataframe['PCA_1'].to_numpy())
-                tracker.update_progress()
+    logging.info("Data loaded") 
+    logging.info("Clustering first dimension")
 
-        tdigest_model_path = os.path.join(config.DATA_PATH, 'tdigest_model.pkl')
-        with open(tdigest_model_path, 'wb') as f:
-            pickle.dump(digest_model, f)
-            logging.info("TDigest model saved: tdigest_model.pkl")
-    else: 
-        with open(args.model, 'rb') as f: 
-            pickle.load(f)
+    # ============================================================
+    # First-level clusters by PCA_1
+    # ============================================================
+    pca_dataframe, _ = cluster.assign_clusters_by_column(pca_dataframe, col='PCA_1', quantiles=config.STEPS_LIST[0], cluster_col="cluster_id")
 
+    # Save first buckets in tmp directory. This will create parquet files that we can then load one by one with Pyspark to continue the clustering from bucke one
+    # into the rest of the buckets
+    # This turns out to be much faster than using dask. I suspect the reason is the graph that dask creates get's too large and complex. 
 
+    logging.info("Saving first buckets to /tmp directory")
+    tmp_directory = os.path.join(config.OUTPUT_PATH, "tmp")
+
+    # Write each bucket from first dimension into a parquet (temporary) file
+    pca_dataframe.to_parquet(tmp_directory, partition_on=["cluster_id_1"])
+
+    # ============================================================
+    # Second-level clusters by PCA_2 within each first-level cluster
+    # ============================================================
+    # For performance reasons the clustering on the following steps is done using spark API instead of Dask. 
+    # TODO: Find a way of doing the whole pipeline in Dask. Probably persisting intermediate results 
+
+    # Initialize SparkSession with optimized configurations
+    spark = SparkSession.builder \
+        .appName("GridClusteringJob") \
+        .master("local[8]") \
+        .config("spark.driver.memory", "50g") \
+        .config("spark.executor.memory", "50g") \
+        .config("spark.sql.shuffle.partitions", "2000") \
+        .config("spark.executor.extraJavaOptions", "-XX:+UseG1GC") \
+        .config("spark.local.dir", "/mnt/10tb_hdd/pyspark_tmp") \
+        .getOrCreate()  
+
+    # For every step in the first dim (i.e. for every bucket created in PCA_1 dimension), create new buckets
+    for first_dim_step in range(config.STEPS_LIST[0]):
+        print(f"Clustering bucket: {first_dim_step}")
+        first_dim_path = os.path.join(tmp_directory, f"./cluster_id_1={first_dim_step}")
+        bucket_dataframe = spark.read.parquet(first_dim_path)  
+
+        # Repartition the DataFrame for better parallelism
+        bucket_dataframe = bucket_dataframe.repartition(2000)
+
+        #  Second of Clustering on PCA_2
+        window_pca2 = Window.orderBy("PCA_2")
+        bucket_dataframe = bucket_dataframe.withColumn("bin_PCA2", ntile(first_dim_step).over(window_pca2) - 1)
+
+        # Repartition based on 'bin_PCA2' before next window function
+        bucket_dataframe = bucket_dataframe.repartition("bin_PCA2")
+
+        # Third Level of Clustering on PCA_3
+        window_pca3 = Window.partitionBy("bin_PCA2").orderBy("PCA_3")
+        bucket_dataframe = bucket_dataframe.withColumn("bin_PCA3", ntile(first_dim_step).over(window_pca3) - 1)
+
+        # Convert bin_PCA2 and bin_PCA3 to integer type if necessary
+        bucket_dataframe = bucket_dataframe.withColumn("bin_PCA2", col("bin_PCA2").cast("int"))
+        bucket_dataframe = bucket_dataframe.withColumn("bin_PCA3", col("bin_PCA3").cast("int"))
+
+        # Create cluster_id with the formula incorporating first_dim_step as bin_PCA1
+        bucket_dataframe = bucket_dataframe.withColumn(
+            "cluster_id",
+            lit(first_dim_step) * (50 ** 2) + col("bin_PCA2") * 50 + col("bin_PCA3")
+        )
+
+        bucket_dataframe_result = bucket_dataframe.select("smiles", "PCA_1", "PCA_2", "PCA_3", "cluster_id")
+
+        # Write each iteration's DataFrame to a separate folder
+        # The final directory structure: /mnt/10tb_hdd/results/bucket_i/
+        print(f"Writing dataframe")
+        output_path = f"/mnt/10tb_hdd/buckets_pca_dask_test/pyspark_combined_results/bucket_{first_dim_step}"
+        bucket_dataframe_result.coalesce(10).write.mode("overwrite").parquet(output_path)
+
+        # TODO: Remove temporary directory? 
+        del bucket_dataframe, bucket_dataframe_result
